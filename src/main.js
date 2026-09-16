a796b70625da46a43f850f0e767dfdeb0ed963b5
ferrarissound-design <ferrarissound@gmail.com>
2026-09-16 17:01:43 +0900

    背景選択ロックを読み込む

--- src/main.js
+++ src/main.js
@@ -5,3 +5,4 @@ import './moving-platform-polish.js?v=20260916-moving-platform-size-4';
 import './moving-platform-collision.js?v=20260916-platform-solid-bounce-2';
 import './mountain-background.js?v=20260916-anime-tree-depth-2';
 import './app.js?v=20260916-left-jump-1';
+import './background-selection-lock.js?v=20260916-background-lock-1';
