d83d552755917ff1b054998001e18079549db4a1
ferrarissound-design <ferrarissound@gmail.com>
2026-09-16 13:50:53 +0900

    Load anime stage-part skin

--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
 import './engine-fixes.js';
 import './performance-fixes.js?v=20260916-player-stutter-1';
+import './anime-stage-parts.js?v=20260916-adventure-parts-1';
 import './app.js?v=20260916-left-jump-1';
